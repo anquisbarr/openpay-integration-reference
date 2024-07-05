f2d2f58c25a2cafddfc091ed409f28ec5c1988e8
Sebastian Quispe <sebastian.quispe.b@utec.edu.pe>
2024-07-05 00:13:04 -0500

    feat: added devicesessionid as a higher component state and tokenId aswell

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import type React from "react";
+import { useState } from "react";
 import "./App.css";
 import OpenPayComponent from "./OpenPayComponent";
+import OpenPayWrapper from "./OpenPayWrapper";
 
 const App: React.FC = () => {
+	const [deviceSessionId, setDeviceSessionId] = useState<string>("");
+	const [tokenId, setTokenId] = useState<string>("");
+
 	return (
 		<div className="App">
-			<h1>Prueba de OpenPay en React</h1>
-			{/* Render the OpenPay component */}
-			<OpenPayComponent />
+			<h1>OpenPay Integration</h1>
+			<OpenPayWrapper setDeviceSessionId={setDeviceSessionId} />
+			<OpenPayComponent
+				deviceSessionId={deviceSessionId}
+				setTokenId={setTokenId}
+			/>
 		</div>
 	);
 };
